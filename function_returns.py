8395b9151958b89b324879bd68ab8eb627a5a54b
Lonpsy <oolalonpe@gmail.com>
2025-04-03 11:16:22 +0200

    updated the code ensuring each function does one thing

--- function_returns.py
+++ function_returns.py
@@ -1,16 +1,15 @@
-def farenheit_conversion(temperature):
+def farenheit_conversion(celcius_temperature):
   '''calculates the fahrenheit temperature '''
-  farenheit_temperature = temperature * 9 / 5 + 32
+  farenheit_temperature = celcius_temperature * 9 / 5 + 32
   farenheit_temperature = round(farenheit_temperature)
   return (f'{farenheit_temperature}')
 
 
-def receive_details():
-  city = input('enter your current city ')
-  temperature = int(input('enter your temperature in degree celcius '))
+def message():
   farenheit = farenheit_conversion(temperature)
   message = f'It is currently {temperature}ºC ({farenheit}ªF) in {city}.'
   print(message)
 
-
-receive_details()
+city = input('enter your current city ')
+temperature = int(input('enter your temperature in degree celcius '))
+message()
